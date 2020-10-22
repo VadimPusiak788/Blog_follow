4fe862de7514e07672797e9176d528cc8f790d00
Vadim788 <vpusak629@gmail.com>
2020-10-22 23:04:27 +0300

    Add read post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import post_following_profiles, DetailPostView, read_post
+from .views import post_following_profiles, DetailPostView, read_post, CreatePost
 
 app_name = 'blog'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('post/', post_following_profiles, name='post_following'),
     path('post/<int:pk>/', DetailPostView.as_view(), name='post_detail'),
     path('choise_read/', read_post, name='read_post'),
+    path('post/create_post/', CreatePost.as_view(), name="create_post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from account.models import Profile
 from itertools import chain
-from django.views.generic import DetailView, View
+from django.views.generic import DetailView, FormView
 from .models import Post
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import PostForm
 
 
 def post_following_profiles(request):
@@ -15,6 +19,7 @@ def post_following_profiles(request):
         posts_user = p.post_set.all()
         posts.append(posts_user)
 
+    posts.append(profile.post_set.all())
     if len(posts) > 0:
         qs = sorted(chain(*posts), reverse=True, key=lambda obj: obj.created)
     return render(request, 'blog/main.html', {'profile': profile, 'posts': qs})
@@ -35,12 +40,21 @@ def read_post(request):
     if request.method == "POST":
         pk = request.POST.get('post_pk')
         post = Post.objects.get(pk=pk)
-        if post.read:
-            post.read = False
-            post.save()
-        else:
+        if not post.read:
             post.read = True
             post.save()
         return redirect('blog:post_following')
 
     return redirect('blog:read_post')
+
+
+class CreatePost(CreateView, LoginRequiredMixin):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'blog/create_post.html'
+    success_url = '/post/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        form.instance.read = True
+        return super().form_valid(form)
